4d571722d9503f20621d0e1a76291fabf174259e
Ruslan Zakirov <Ruslan.Zakirov@gmail.com>
2009-01-17 17:09:35 +0300

    * most probably we don't need this

--- lib/Globus/Model/DB.pm
+++ lib/Globus/Model/DB.pm
@@ -5,19 +5,19 @@ use base 'Catalyst::Model::DBIC::Schema';
 
 __PACKAGE__->config(
 	schema_class => 'Globus::DB',
-		connect_info => [sub {
-			my ($dsn, $user, $pass) = @{Globus->config->{DB}};
-			my $dbh=DBI->connect($dsn, $user, $pass,{ autocommit=>1 });
-			if ( $dsn =~ /SQLite/ ) {
-				$dbh->{unicode}=1;
-			} elsif ( $dsn =~ /mysql/ ) {
-				$dbh->{mysql_enable_utf8}=1;
-				$dbh->do('set names utf8;');
-				$dbh->do('set character set utf8;');
-			}
-			return $dbh;
-		}
-	],
+# ruz: most probably it's not required as we have this in our config
+#		connect_info => [sub {
+#			my ($dsn, $user, $pass) = @{Globus->config->{DB}};
+#			my $dbh=DBI->connect($dsn, $user, $pass,{ autocommit=>1 });
+#			if ( $dsn =~ /SQLite/ ) {
+#				$dbh->{unicode}=1;
+#			} elsif ( $dsn =~ /mysql/ ) {
+#				$dbh->{mysql_enable_utf8}=1;
+#				$dbh->do('set names utf8;');
+#				$dbh->do('set character set utf8;');
+#			}
+#			return $dbh;
+#		} ],
 );
 
 
