2482c26f4c351869777f0a8a425a10db1434be57
ruz <cubic@cubic-pc.localdomain>
2009-01-19 01:52:26 +0300

    * add some feeds on deploy

--- script/globus_deploy.pl
+++ script/globus_deploy.pl
@@ -8,5 +8,11 @@ use YAML qw(Dump Bless);
 use Globus::DB;
 
 my $schema=Globus::DB->connect( Globus::DB->our_connect_handler );
-
 $schema->deploy({ add_drop_table => 1, },'');
+
+$schema->populate( Feed => [
+    [qw(link)],
+    ['http://perl6.ru/rss/'],
+    ['http://dr-japh.livejournal.com/data/rss'],
+    ['http://feeds.delicious.com/v2/rss/tag/russian+perl?count=15'],
+]);
