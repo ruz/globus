11cf96e65d2643099cd44bd9c0035e87ee625be6
ruz <cubic@cubic-pc.localdomain>
2009-01-19 17:48:22 +0300

    * update list of feeds with everything listed on our wiki, hoever some   are commented

--- script/globus_deploy.pl
+++ script/globus_deploy.pl
@@ -15,4 +15,16 @@ $schema->populate( Feed => [
     ['http://perl6.ru/rss/'],
     ['http://dr-japh.livejournal.com/data/rss'],
     ['http://feeds.delicious.com/v2/rss/tag/russian+perl?count=15'],
+    ['http://moscow.pm.org/rss/'],
+    ['http://kostenko.name/feed/'],
+    ['http://blog.bessarabov.ru/feed/'],
+
+#http://www.perl-blog.de/
+#http://blog.seo-p.ru/
+#http://perl.blogit.ru/
+#http://blog.gravatar.com
+#http://use.perl.org/~andy.sh/journal/
+#http://use.perl.org
+#http://blog.yandex.ru/search.rss?text=%D1%85%D0%B0%D0%BA%D0%BC%D0%B8%D1%82 
+#http://community.livejournal.com/ru_perl
 ]);
