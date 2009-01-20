63cee24d9af84eba2185ab010b3c2183d29bee47
ruz <cubic@cubic-pc.localdomain>
2009-01-20 08:57:27 +0300

    * item and tag in ItemTag shouldn't have any defaults, it's PK

--- lib/Globus/DB/ItemTag.pm
+++ lib/Globus/DB/ItemTag.pm
@@ -8,13 +8,13 @@ use base 'DBIx::Class';
 __PACKAGE__->load_components("Core");
 __PACKAGE__->table("items_tags");
 __PACKAGE__->add_columns(
-  item => { data_type => "INTEGER", default_value => 0, is_nullable => 0},
-  tag => { data_type => "INTEGER", default_value => 0, is_nullable => 0},
+  item => { data_type => "INTEGER", default_value => undef, is_nullable => 0},
+  tag => { data_type => "INTEGER", default_value => undef, is_nullable => 0},
 );
 
 __PACKAGE__->set_primary_key(qw(item tag));
-__PACKAGE__->add_unique_constraint("item_tag_item_tag_Idx", [qw[item tag]]);
 
-__PACKAGE__->belongs_to("item", "Globus::DB::Item", { 'foreign.id' => "self.item" });
-__PACKAGE__->belongs_to("tag", "Globus::DB::Tag", { 'foreign.id' => "self.tag" });
+__PACKAGE__->belongs_to("item", "Globus::DB::Item", { 'foreign.id' => 'self.item' });
+__PACKAGE__->belongs_to("tag", "Globus::DB::Tag", { 'foreign.id' => 'self.tag' });
+
 1;
