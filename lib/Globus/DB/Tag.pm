cb64507cc4c7614c8f69fe3ca1159e65efa45cd4
ruz <cubic@cubic-pc.localdomain>
2009-01-20 09:03:26 +0300

    * add indexes on en and ru versions

--- lib/Globus/DB/Tag.pm
+++ lib/Globus/DB/Tag.pm
@@ -15,7 +15,23 @@ __PACKAGE__->add_columns(
 
 __PACKAGE__->utf8_columns(qw/en ru klingon/);
 __PACKAGE__->set_primary_key("id");
+__PACKAGE__->add_unique_constraint("tag_en_Idx", ["en"]);
+__PACKAGE__->add_unique_constraint("tag_ru_Idx", ["ru"]);
 
-__PACKAGE__->has_many('doctags', 'Globus::DB::ItemTag', 'tag');
-__PACKAGE__->many_to_many(items => doctags => 'item');
+__PACKAGE__->has_many('item_tag', 'Globus::DB::ItemTag', 'tag');
+__PACKAGE__->many_to_many(items => item_tag => 'item');
+
+use Text::Unidecode;
+
+sub new {
+    my ( $class, $attrs ) = @_;
+
+    unless ( defined $attrs->{'en'} && length $attrs->{'en'} ) {
+        my ($from) = grep defined && length, map $attrs->{$_}, qw(ru klingon);
+        $attrs->{'en'} = unidecode( $from );
+    }
+
+    return $class->next::method($attrs);
+}
+
 1;
