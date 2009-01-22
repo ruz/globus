9f993346c904abdbf2ad6086aa988eead8090bf7
ruz <cubic@cubic-pc.localdomain>
2009-01-22 07:12:31 +0300

    * add config column for Feed

--- lib/Globus/DB/Feed.pm
+++ lib/Globus/DB/Feed.pm
@@ -9,10 +9,20 @@ __PACKAGE__->add_columns(
   id => { data_type => "INTEGER", default_value => undef, is_nullable => 0, is_auto_increment => 1},
   link => { data_type => "VARCHAR", default_value => undef, is_nullable => 0, size => 255, },
   title => { data_type => "VARCHAR", default_value => undef, is_nullable => 1, size => 255, },
+  config => { data_type => "TEXT", default_value => undef, is_nullable => 1, },
+
 );
 
 __PACKAGE__->utf8_columns(qw/title/);
 __PACKAGE__->set_primary_key("id");
+
+use YAML qw();
+__PACKAGE__->inflate_column(
+    config => {
+        'inflate'   => sub { YAML::Load($_[0]) },
+        'deflate'   => sub { YAML::Dump($_[0]) },
+    }
+);
 
 1;
 
