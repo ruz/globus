916701ceb591972f92517054aaede7f92c65b50c
ruz <cubic@cubic-pc.localdomain>
2009-01-22 07:14:10 +0300

    * add more feeds to deploy, show examle config option

--- script/globus_deploy.pl
+++ script/globus_deploy.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl -w
 
 use strict;
+use warnings;
+use utf8;
 use ex::lib "../lib";
 use Data::Dumper;
 use YAML qw(Dump Bless);
@@ -11,13 +13,21 @@ my $schema=Globus::DB->connect( Globus::DB->our_connect_handler );
 $schema->deploy({ add_drop_table => 1, },'');
 
 $schema->populate( Feed => [
-    [qw(link)],
+    [qw(link config)],
     ['http://perl6.ru/rss/'],
     ['http://dr-japh.livejournal.com/data/rss'],
     ['http://feeds.delicious.com/v2/rss/tag/russian+perl?count=15'],
     ['http://moscow.pm.org/rss/'],
     ['http://kostenko.name/feed/'],
     ['http://blog.bessarabov.ru/feed/'],
+    ['http://feeds.feedburner.com/DevelopersOrgUa'],
+    ['http://habrahabr.ru/rss/tag/perl/'],
+    ['http://www.teroff.ru/feed', <<END],
+---
+filter: tag:perl
+END
+    ['http://dzhariy.blogspot.com/feeds/posts/default'],
+    ['http://php-coder.livejournal.com/data/atom'],
 
 #http://www.perl-blog.de/
 #http://blog.seo-p.ru/
